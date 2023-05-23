06023ad7d8271fb2e0f0df7c575c65bca3e05339
Claire McGuigan <mcguigan.claire@outlook.com>
2023-05-22 23:21:18 -0400

    attaching footer logos

--- personal-website/src/components/Footer/Footer.js
+++ personal-website/src/components/Footer/Footer.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './Footer.css';
-import LinkedIn from '../../assests/images/LinkedIn.jpg';
+import linkedIn from '../../assests/images/linkedIn3.png';
+import github from '../../assests/images/github.png';
 
 export default function Footer(){
     return (
         <div id='footer'>
-            <div id='main-footer'>
+            <div className='footer-logo-container'>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
                 <a href="mailto:mcguigan.claire@outlook.com" className="material-symbols-outlined">mail</a>
             </div>
-            <div id='linkedin-footer'>
-                <a href="https://www.linkedin.com/in/claire-mcguigan/" target="_blank">{LinkedIn}</a>
+            <div className='footer-logo-container'>
+                <a href="https://www.linkedin.com/in/claire-mcguigan/" target="_blank"><img className="footer-img" src={linkedIn}/></a>
             </div>
-            <div id='github-footer'>
-                <a href="" target="_blank"></a>
+            <div className='footer-logo-container'>
+                <a href="" target="_blank"><img className="footer-img" src={github}/></a>
             </div>
         </div>
     );
