61c025c1a7db0437cb6c6a5a4e93d8335df1aa37
Claire McGuigan <mcguigan.claire@outlook.com>
2023-05-28 23:56:56 -0400

    moving data
    moving project descriptions and experience descriptions to separate js files

--- personal-website/src/components/Experience/ExperienceSection.js
+++ personal-website/src/components/Experience/ExperienceSection.js
@@ -1,62 +1,11 @@
 import React, { useState } from 'react';
 import Experience from './Experience';
 import './ExperienceSection.css';
-import RBC from '../../assests/images/RBC_icon.jpg';
-import WDSLogo from '../../assests/images/WDS.jpg';
-import Bell from '../../assests/images/Bell.png';
-import WETT from '../../assests/images/WETT.jpg';
+import { extraCurr } from '../../data/ExperienceData';
+import { professional } from '../../data/ExperienceData';
 
 export default function ExperienceSection(props){
   const [isToggled, setIsToggled] = useState(false);
-  const [professional, setProfessional] = useState([
-    {
-      imgURL: RBC, 
-      title: "Developer, RBC Amplify", 
-      date: "May 2023 - August 2023", 
-      description: `RBC's Amplify program is a summer internship 
-      where students are put into groups of 4 and given challenge 
-      prompts to solve over the summer. Together, my team and I went
-      through all the phases of the software development process, 
-      from breaking down the problem statement, researching the problem,
-      brainstoring potential solutions, designing, and implementing.`,
-      link: ''
-    },
-    {
-      imgURL: Bell, 
-      title: "Access Network Engineering Student", 
-      date: "May 2022 - December 2022", 
-      description: `Co-Lead Developer of Cobra, a web app for Bell's Contractor Management Team. 
-      Lead the development of a Django web app used by Bell's internal contract management team 
-      to manage vendor contracts, contract SKUs, tracking contractor's access to Bell sites and more.
-      Worked full time from May to September and then stayed on part time until December while at school
-      to continue the development and handoff of the application`,
-      link: ''
-    }
-  ]);
-  const [extraCurr, setExtraCurr] = useState([
-    {
-      imgURL: WDSLogo, 
-      title: "Developer Team Lead, Western Developers Society", 
-      date: "Oct 2022 - April 2023", 
-      description: `Team Lead of a group of 8 developers for a club
-      at Western University that pairs groups of developers with
-      non-profits that need help developing their websites and other
-      software development related services. As Team Lead, I was responsible
-      for developing high level timelines, communicating with stakeholders,
-      requirements elicitations and ensuring my team stayed on track 
-      to meet deadlines`,
-      link: 'https://london-bridges.vercel.app/team'
-    },
-    {
-      imgURL: WETT,
-      title: "Head of Development",
-      date: "Oct 2022 - April 2023",
-      description: `Head of software development for Western Engineering Tobogganing Team, 
-      a Western University engineering club. Includes developing and redesigning the club's 
-      website and managing a team of developers.`,
-      link: 'https://wetterisbetter.ca/'
-    }
-  ]);
 
   const toggleExperience = () => {
     setIsToggled((prev) => !prev);
---
+++ personal-website/src/data/ProjectData.js
@@ -0,0 +1,23 @@
+export const projects = [
+    {
+        name: 'MangoHealth',
+        description: `At CalgaryHacks 2022 Hackathon, in a team of 5, developed a React Native 
+        app that scans food item barcodes and uses the EDAMAM Food Database API to get its 
+        nutrition facts. Comparisons are then made to help the user truly understand what, 
+        for example, 20g of sugar is in comparison to a can of pop. Developed with both a Flask 
+        and Node.js server, and a React Native application front-end.`
+    },
+    {
+        name: 'Co-Living',
+        description: `In a team of 4, at SheHacks 6, 2021 Hackathon, developed an organizational 
+        living website using JavaScript, HTML and CSS for roommates to keep track of shared 
+        expenses, joint and individual grocery lists and a chore chart that randomly assigns
+        roommates to customizable chores`
+    },
+    {
+        name: 'Code4ACause',
+        description: `In a team of 4 at eHacks 2022 Hackathon, developed a website using HTML, 
+        CSS and JavaScript that connects young programmers looking to develop their skills with 
+        non-profits looking for help with website building, app creation…etc.`
+    },
+]
