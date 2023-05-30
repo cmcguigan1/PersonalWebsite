dfb49683cc3fa24d836a2c71c0d82a0c95ccf066
Claire McGuigan <mcguigan.claire@outlook.com>
2023-05-29 22:34:26 -0400

    fixing Description so it's passing state properly

--- personal-website/src/components/Description/Description.js
+++ personal-website/src/components/Description/Description.js
@@ -2,18 +2,18 @@ import React, {useEffect} from 'react';
 import './Description.css';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-export default function Description(){
-    const location=useLocation();
-    const { data }= location.state;
+export default function Description(props){
+    const location = useLocation();
+    const { data } = location.state;
 
     useEffect(() => {
+        console.log(location.state);
         console.log(data);
     });
 
     return (
         <div id='description'>
-            <h1>Hi</h1>
-            { data && <h1>{data}</h1>}
+            <h1>{data.title}</h1>
         </div>
     );
 }
