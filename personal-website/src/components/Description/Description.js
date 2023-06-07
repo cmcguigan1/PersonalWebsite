65b6789b6ba49a2ec24cc93cae971a27fb9bd132
Claire McGuigan <mcguigan.claire@outlook.com>
2023-06-07 01:21:48 -0400

    Adding Course Outline Management Tool Project

--- personal-website/src/components/Description/Description.js
+++ personal-website/src/components/Description/Description.js
@@ -10,6 +10,7 @@ export default function Description(props){
 
     useEffect(() => {
         document.title = 'Claire McGuigan';
+        window.scrollTo(0, 0);
     });
 
     return (
--- personal-website/src/data/ProjectData.js
+++ personal-website/src/data/ProjectData.js
@@ -17,32 +17,55 @@ import CoLiving4 from '../assests/images/CoLiving/CoLiving4.png';
 import CoLiving5 from '../assests/images/CoLiving/CoLiving5.png';
 import CoLiving6 from '../assests/images/CoLiving/CoLiving6.png';
 
+import COMT1 from '../assests/images/CourseOutlineManagementTool/COMT1.png';
+import COMT15 from '../assests/images/CourseOutlineManagementTool/COMT15.png';
+import COMT16 from '../assests/images/CourseOutlineManagementTool/COMT16.png';
+import COMT10 from '../assests/images/CourseOutlineManagementTool/COMT10.png';
+import COMT11 from '../assests/images/CourseOutlineManagementTool/COMT11.png';
+import COMT12 from '../assests/images/CourseOutlineManagementTool/COMT12.png';
+import COMT13 from '../assests/images/CourseOutlineManagementTool/COMT13.png';
+import COMT17 from '../assests/images/CourseOutlineManagementTool/COMT17.png';
+import COMT19 from '../assests/images/CourseOutlineManagementTool/COMT19.png';
+import COMT22 from '../assests/images/CourseOutlineManagementTool/COMT22.png';
+import COMT23 from '../assests/images/CourseOutlineManagementTool/COMT23.png';
+import COMT24 from '../assests/images/CourseOutlineManagementTool/COMT24.png';
+import COMT2 from '../assests/images/CourseOutlineManagementTool/COMT2.png';
+import COMT4 from '../assests/images/CourseOutlineManagementTool/COMT4.png';
+import COMT20 from '../assests/images/CourseOutlineManagementTool/COMT20.png';
+import COMT21 from '../assests/images/CourseOutlineManagementTool/COMT21.png';
+import COMT5 from '../assests/images/CourseOutlineManagementTool/COMT5.png';
+import COMT6 from '../assests/images/CourseOutlineManagementTool/COMT6.png';
+import COMT7 from '../assests/images/CourseOutlineManagementTool/COMT7.png';
+
+
 export const projects = [
-    /*{
+    {
         id: 1,
         type: 'school',
-        title: 'Teamify',
-        date: "December 2022",
+        title: 'Course Outline Management Tool',
+        date: "January 2023 - April 2023",
         location: '',
-        company: 'Final Project - Web Technologies course',
-        companyDescription: `Completed 3316A Web Technologies course in first semester of 3rd year 
-        of software engineering. Course covered topics about building full-stack applications with a 
-        proper REST API, proper authentication, and security`,
-        description: `Designed, implemented, tested, and deployed a 
-        full stack React app music streaming platform. Users are able to search 
-        through tracks, make playlists, view public playlists and leave 
-        ratings. Utilized Express Node.js backend, MySQL database and 
-        deployed on AWS. Link to Project`,
-        technologies: 'React, Node.js, Express.js, MySQL, AWS, Firebase',
-        link: 'https://github.com/cmcguigan1/Teamify',
+        company: 'Project - Software Engineering Design Project course',
+        companyDescription: `Completed 3350B Software Engineering Design course in second semester of 3rd year 
+        of software engineering. Course was a project based course where team had to iterate all stages of
+        the software development lifecycle`,
+        description: `A web app created in my 3350B project based course that is a course outline management tool
+        for instructors and admin personnel. Intructors can create outlines for courses they are assigned to, view 
+        previous course outlines for that course, keep version history, and submit outlines to admin to be approved.
+        Once outlines are approved, they can then print of the approved versions. Admin can assign instructors to courses,
+        view submitted outlines and either approve or send it back to the instructor with comments`,
+        technologies: 'React, Node.js, Express.js, MySQL, GCP',
+        link: '',
         keyPoints: [
-            `Worked with a partner, another software engineering student`,
-            `Worked on the front end, developed the backend API, database, and deployment`,
-            `Partner worked on integrating the Firebase authentication and the login page front end`,
-            `Received an overall mark of 120% on the project`
+            `Worked in a team with 5 other students`,
+            `Planned and executed bi-weekly sprints`,
+            `Participated in weekly stand-ups in front of team members and stakeholders to discuss progress, roadblocks and concerns`,
+            `Iterated all steps of the software development lifecycle including requirements elicitaiton, 
+            requirments analysis, system design, object design, implementation, and testing`,
+            `Received an overall mark of 90% in the course`
         ],
-        images: [Teamify1, Teamify2, Teamify3, Teamify4, Teamify5, Teamify6, Teamify7, Teamify8, Teamify9, Teamify10, Teamify11]
-    },*/
+        images: [COMT1, COMT15, COMT16, COMT10, COMT11, COMT12, COMT13, COMT17, COMT19, COMT22, COMT23, COMT24, COMT2, COMT4, COMT20, COMT21, COMT5, COMT6, COMT7]
+    },
     {
         id: 2,
         type: 'school',
@@ -57,7 +80,7 @@ export const projects = [
         full stack React app music streaming platform. Users are able to search 
         through tracks, make playlists, view public playlists and leave 
         ratings. Utilized Express Node.js backend, MySQL database and 
-        deployed on AWS. Link to Project`,
+        deployed on AWS`,
         technologies: 'React, Node.js, Express.js, MySQL, AWS, Firebase',
         link: 'https://github.com/cmcguigan1/Teamify',
         keyPoints: [
